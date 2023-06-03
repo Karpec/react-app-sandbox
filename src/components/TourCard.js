85c059e56418da39b02338e94d19c3d1b4bc8e0b
Adam Karpita <adam.karpita@trayto.com>
2023-06-03 13:15:53 +0200

    WIP

---
+++ src/components/TourCard.js
@@ -0,0 +1,20 @@
+import {Paper} from "@mui/material";
+import {Grid} from "@mui/material";
+import {Typography} from "@mui/material";
+
+const TourCard = () => {
+    return (
+    <Grid item xs={3}>
+        <Paper elevetation={3} variant="outlined">
+            <img src='https://www.xemel.cz/assets/front/images/screens/screen-edit-tree.png' alt='' className='img' />
+            <Typography
+
+
+
+
+        </Paper>
+    </Grid>
+    );
+};
+
+export default TourCard
